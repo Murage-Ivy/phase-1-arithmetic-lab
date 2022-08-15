305905062405bb5a6c72d61e05a8b74efc91dfd3
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-08-15 12:21:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// Write your code here,
+// used the multiplication operator
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// used Math.random and Maht.ceil
+let random = Math.ceil((Math.random() * 10) + 1);
+
+// used the modulus operator
+let num3 = 64;
+let num4 = 6;
+let mod = num3 % num4;
+
+// use Math.max to find the highest number in a set
+let max = Math.max(15, 18, 20, -1, 8);
+
